42a07310e411a6c506496b9eb13aa1889c7ca256
Mario Rodriguez Martinez <mariog.rodriguezm@gmail.com>
2023-02-13 09:51:55 +0200

    Iteraciones listas, falta CSS

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+const MoviesModel= require ("../models/Movie.model.js");
+const { route } = require ('../app.js');
 
-router.get('/movies', (req, res, next) => {
-    Movie
-        .find()
-        .then( movies => res.render('movies', {movies}))
-        .catch(err => console.log(err))
+/* GET home page */
+router.get('/', (req, res, next) => {
+    res.render('index')
 });
 
+router.get("/movies", (req,res,next)=>{
+    MoviesModel.find()
+    .then((response)=>{
+        console.log(response)
+        res.render("movies.hbs" , {
+            allMovies: response
+        })
+    })
+    .catch((error)=>{
+        next (error)
+    })
 
 
-
+    router.get("/movies/:moviesById",(req,res,next)=>{
+        const{ moviesById } = req.params
+        
+        MoviesModel.findById( moviesById )
+        .then((response)=>{
+            // console.log(response)
+            res.render("moviesById.hbs",{
+                moviesID : response
+            })
+        })
+        .catch((err)=>{
+            next (err)
+        })
+    })
+    
+    
+    
+})
 module.exports = router;
